41afb8f4e936ffd3b57a3e938eaf285ca205eef4
Crauzer <filip.quitko@gmail.com>
2019-08-17 23:38:19 +0200

    moved content loading to separate class

---
+++ Ripple/ContentLoader.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Fantome.Libraries.League.IO.BIN;
+using Fantome.Libraries.League.IO.MapGeometry;
+using HelixToolkit.Wpf.SharpDX;
+using HelixToolkit.Wpf.SharpDX.Model.Scene;
+using Imaging.DDSReader;
+using SharpDX;
+
+using mPoint3D = System.Windows.Media.Media3D.Point3D;
+using mVector3D = System.Windows.Media.Media3D.Vector3D;
+using dxVector3 = SharpDX.Vector3;
+using dxVector2 = SharpDX.Vector2;
+
+namespace Ripple
+{
+    public static class ContentLoader
+    {
+        public static SceneNodeGroupModel3D LoadMapMGEO(string fileLocation)
+        {
+            return LoadMapMGEO(new MGEOFile(fileLocation));
+        }
+
+        public static SceneNodeGroupModel3D LoadMapMGEO(MGEOFile mgeo)
+        {
+            List<MeshGeometryModel3D> meshGeometryModels = GenerateMeshGeometryModels(mgeo);
+            SceneNodeGroupModel3D mainModelGroup = new SceneNodeGroupModel3D();
+
+            foreach (MeshGeometryModel3D meshGeometryModel in meshGeometryModels)
+            {
+                GroupNode groupNode = new GroupNode()
+                {
+                    Name = meshGeometryModel.Name
+                };
+
+                groupNode.AddChildNode(new MeshNode()
+                {
+                    Name = meshGeometryModel.Name,
+                    Geometry = meshGeometryModel.Geometry,
+                    Material = meshGeometryModel.Material,
+                });
+
+                mainModelGroup.AddNode(groupNode);
+            }
+
+            return mainModelGroup;
+        }
+
+        public void LoadMapBIN(string fileLocation)
+        {
+
+        }
+
+        private static List<MeshGeometryModel3D> GenerateMeshGeometryModels(MGEOFile mgeo)
+        {
+            List<MeshGeometryModel3D> models = new List<MeshGeometryModel3D>(mgeo.Objects.Count);
+
+            foreach (MGEOObject mgeoModel in mgeo.Objects)
+            {
+                IntCollection indices = new IntCollection(mgeoModel.Indices.Select(x => (int)x).AsEnumerable());
+                Vector3Collection vertices = new Vector3Collection(mgeoModel.Vertices.Count);
+                Vector2Collection uvs = new Vector2Collection(mgeoModel.Vertices.Count);
+                foreach (MGEOVertex vertex in mgeoModel.Vertices)
+                {
+                    vertices.Add(new dxVector3(vertex.Position.X, vertex.Position.Y, vertex.Position.Z));
+                    uvs.Add(new dxVector2(vertex.DiffuseUV.X, vertex.DiffuseUV.Y));
+                }
+
+                MeshGeometry3D meshGeometry = new MeshGeometry3D()
+                {
+                    Indices = indices,
+                    Positions = vertices,
+                    TextureCoordinates = uvs
+                };
+
+                MeshGeometryModel3D model = new MeshGeometryModel3D()
+                {
+                    Geometry = meshGeometry,
+                    Material = DiffuseMaterials.Pearl,
+                    Name = mgeoModel.Name
+                };
+
+                models.Add(model);
+            }
+
+            return models;
+        }
+
+        public static MemoryStream DDSToImage(string fileLocation)
+        {
+            return DDSToImage(File.ReadAllBytes(fileLocation));
+        }
+
+        public static MemoryStream DDSToImage(byte[] dds)
+        {
+            MemoryStream ms = new MemoryStream();
+            DDS.LoadImage(dds).Save(ms, ImageFormat.Png);
+
+            return ms;
+        }
+    }
+}
--- Ripple/MainWindow.xaml.cs
+++ Ripple/MainWindow.xaml.cs
@@ -1,32 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using Fantome.Libraries.League.IO.WorldGeometry;
-using HelixToolkit.Wpf.SharpDX.Core;
-using System.IO;
-using Imaging.DDSReader;
-using System.Drawing.Imaging;
-using Microsoft.Win32;
-using HelixToolkit.Wpf.SharpDX.Render;
+﻿using Fantome.Libraries.League.IO.MapGeometry;
 using HelixToolkit.Wpf.SharpDX;
+using HelixToolkit.Wpf.SharpDX.Model.Scene;
+using Imaging.DDSReader;
+using Microsoft.Win32;
+using SharpDX;
+using System.Collections.Generic;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Windows;
 
 using mPoint3D = System.Windows.Media.Media3D.Point3D;
 using mVector3D = System.Windows.Media.Media3D.Vector3D;
-using dxPoint3D = SharpDX.Point;
-using HelixToolkit.Wpf.SharpDX.Model.Scene;
-using Fantome.Libraries.League.IO.MapGeometry;
-using SharpDX;
 
 namespace Ripple
 {
@@ -47,6 +32,7 @@ namespace Ripple
             this.Camera = CreateCamera();
             this.Viewport.DataContext = this;
 
+
         }
             
 
@@ -58,82 +44,9 @@ namespace Ripple
 
             if (dialog.ShowDialog() == true)
             {
-                LoadMGEO(dialog.FileName);
+                LoadMapMGEO(dialog.FileName);
             }
 
-
-        }
-
-        public void LoadMGEO(string fileLocation)
-        {
-            MGEOFile mgeo = new MGEOFile(fileLocation);
-            List<MeshGeometryModel3D> meshGeometryModels = GenerateMeshGeometryModels(mgeo);
-            /*elements.Add(new DirectionalLight3D()
-            {
-                Direction = new Vector3(0, -1, -0.5f),
-                Color = new Color4(1, 1, 1, 1)
-            });
-            elements.Add(new AmbientLight3D()
-            {
-                Color = new Color(0.64705882352f, 0.64705882352f, 0.64705882352f, 1)
-            });*/
-
-            foreach(MeshGeometryModel3D meshGeometryModel in meshGeometryModels)
-            {
-                GroupNode groupNode = new GroupNode()
-                {
-                    Name = meshGeometryModel.Name
-                };
-
-                groupNode.AddChildNode(new MeshNode()
-                {
-                    Name = meshGeometryModel.Name,
-                    Geometry = meshGeometryModel.Geometry,
-                    Material = meshGeometryModel.Material
-                });
-
-                this.MainModelGroup.AddNode(groupNode);
-            }
-        }
-
-        public List<MeshGeometryModel3D> GenerateMeshGeometryModels(MGEOFile mgeo)
-        {
-            List<MeshGeometryModel3D> models = new List<MeshGeometryModel3D>(mgeo.Objects.Count);
-
-            foreach (MGEOObject mgeoModel in mgeo.Objects)
-            {
-                MeshGeometry3D meshGeometry = new MeshGeometry3D();
-
-                IntCollection indices = new IntCollection(mgeoModel.Indices.Select(x => (int)x).AsEnumerable());
-                Vector3Collection vertices = new Vector3Collection(mgeoModel.Vertices.Count);
-                Vector2Collection uvs = new Vector2Collection(mgeoModel.Vertices.Count);
-                foreach (MGEOVertex vertex in mgeoModel.Vertices)
-                {
-                    vertices.Add(new Vector3(vertex.Position.X, vertex.Position.Y, vertex.Position.Z));
-                    uvs.Add(new Vector2(vertex.DiffuseUV.X, vertex.DiffuseUV.Y));
-                }
-
-                meshGeometry.Indices = indices;
-                meshGeometry.Positions = vertices;
-                meshGeometry.TextureCoordinates = uvs;
-
-                MeshGeometryModel3D model = new MeshGeometryModel3D();
-                model.Geometry = meshGeometry;
-                model.Material = PhongMaterials.Blue;
-                model.Name = mgeoModel.Name;
-
-                models.Add(model);
-            }
-
-            return models;
-        }
-
-        private MemoryStream DDSToImage(string fileLocation)
-        {
-            MemoryStream ms = new MemoryStream();
-            DDS.LoadImage("textures/4x_" + fileLocation).Save(ms, ImageFormat.Png);
-
-            return ms;
 
         }
 
